456905a1c88f2aacb27cd551673bee1df1b8e98f
HackeR <116006991+TeMuRXaliloV@users.noreply.github.com>
2023-03-12 02:43:11 +0500

    Imtihon-5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Components/Header/Header';
+import Home from './Pages/Home/Home';
+import Product from './Pages/Product/Product';
+import Shop from './Pages/Shop/Shop';
+import Like from './Pages/Like/Like';
+import Footer from './Components/Footer/Footer';
+import NotFound from './Pages/NotFound/NotFound';
+import Login from './Components/Auth/Sign/Sign';
+import Sign from './Components/Auth/Login/Login';
 import './App.css';
+import { useNavigate } from "react-router-dom";
+import { useEffect, } from 'react';
+import { Routes , Route } from 'react-router-dom'
+
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path='Login' element={<Login />} />
+        <Route path='Sign' element={<Sign />} />
+        <Route path='/' element={<Home />} />
+        <Route path='Product' element={<Product />} />
+        <Route path='Shop' element={<Shop />} />
+        <Route path='Like' element={<Like />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
